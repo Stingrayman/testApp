169cb38c042a119efe5d2f22273e3d203b1e2386
Lee <masteriosdev@gmail.com>
2013-12-11 13:29:35 +0200

    iOS App
    Test iOS App Bonjour Service Browser

---
+++ bojoureBrowser/BonjourAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BonjourAppDelegate.h
+//  bojoureBrowser
+//
+//  Created by Alik on 12/11/13.
+//  Copyright (c) 2013 Roma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BonjourAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
